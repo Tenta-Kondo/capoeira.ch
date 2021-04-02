6560d458a002f3b73aae1e8ad485afa4ac7fb8c9
Tenta-Kondo <tenta1010verde@gmail.com>
2021-04-02 22:38:15 +0900

    https

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Http\Middleware\ForceHttps;
 
 class Kernel extends ConsoleKernel
 {
@@ -34,8 +35,11 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
+    protected $middleware = [
+        ForceHttps::class, // 追加
+    ];
 }
