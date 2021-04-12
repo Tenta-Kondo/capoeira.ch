dd4a08ada7712df380df3dbeb6732d1a79e23b53
Tenta-Kondo <tenta1010verde@gmail.com>
2021-04-12 19:46:40 +0900

    update-secure

--- resources/views/header-2.blade.php
+++ resources/views/header-2.blade.php
@@ -4,15 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Upright&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    <script src="{{asset('js/style.js')}}"></script>
-    <script src="{{asset('js/animsition.js')}}"></script>
+    <script src="{{secure_asset('js/style.js')}}"></script>
+    <script src="{{secure_asset('js/animsition.js')}}"></script>
     <link rel="stylesheet" href="{{secure_asset('css/animsition.css')}}">
     <title>Document</title>
 </head>
