d2d16c9ef57ba8fda3abdf4f700a432b07238e0a
Tenta-Kondo <tenta1010verde@gmail.com>
2021-04-28 14:36:03 +0900

    update

--- resources/views/subscription/subscript.blade.php
+++ resources/views/subscription/subscript.blade.php
@@ -6,18 +6,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="{{ secure_asset('css/style.css') }}" rel="stylesheet">
-   
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="">
-    
+
 </head>
 
 <body>
-    <div id="app">
+    <div id="app" class="container">
         <h1 class="mb-4">Stripeを使った月額課金・サンプル</h1>
-       
-           
+        <div class="row">
+            <div class="offset-3 col-6">
                 <div class="card mb-4">
-                   
+                    <div class="card-body bg-light">
                         <div v-if="!isSubscribed">
                             <div class="form-group">
                                 <select class="form-control" v-model="plan">
@@ -44,7 +44,7 @@
                             <!-- 課金中 -->
                             <div v-else>
                                 <div class="mb-3">現在、課金中です。</div>
-                                <button class="btn" type="button" @click="cancel">キャンセル</button>
+                                <button class="btn btn-warning" type="button" @click="cancel">キャンセル</button>
                                 <hr>
                                 <div class="form-group">
                                     課金中のプラン： <span v-text="details.plan"></span>
@@ -53,7 +53,7 @@
                                     <select class="form-control" v-model="plan">
                                         <option v-for="(value,key) in planOptions" :value="key" v-text="value"></option>
                                     </select><br>
-                                    <button class="btn" type="button" @click="changePlan">プランを変更する</button>
+                                    <button class="btn btn-success" type="button" @click="changePlan">プランを変更する</button>
                                 </div>
                                 <hr>
                                 <div class="form-group">
@@ -63,24 +63,24 @@
                                     <input type="text" class="form-control" v-model="cardHolderName" placeholder="名義人（半角ローマ字）">
                                 </div>
                                 <div class="form-group">
-                                    <div id="update-card"></div><br>
+                                    <div id="update-card" class="bg-white"></div><br>
                                     <button type="button" class="btn btn-secondary" data-secret="{{ $intent->client_secret }}" @click="updateCard">
                                         クレジットカードを変更する
                                     </button>
                                 </div>
                             </div>
                         </div>
-                   
+                    </div>
                 </div>
                 <div class="card">
-                    
+                    <div class="card-body">
                         <h5>テスト入力について</h5>
                         <hr>
                         <strong>名義人：</strong> 半角ローマ字ならなんでもOK<br>
                         <strong>カード番号：</strong> <a href="https://stripe.com/docs/testing#cards" target="_blank">テスト用のカード番号</a>に用意されています。なお、年／月は未来の日付ならいつでもOKで、CVCも数字ならなんでもOKです。
-                    
-              
-           
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     <script src="https://js.stripe.com/v3/"></script>
