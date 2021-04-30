2cb28e0057c2a26a475af4ee0f9400f51ea19e69
Tenta-Kondo <tenta1010verde@gmail.com>
2021-04-30 17:09:07 +0900

    update

--- app/Http/Controllers/BrogController.php
+++ app/Http/Controllers/BrogController.php
@@ -10,7 +10,7 @@ use App\Models\Comment;
 use App\Models\Image;
 use JD\Cloudder\Facades\Cloudder;
 use Illuminate\Support\Facades\Auth;
-use Stripe\Price;
+use App\Models\Price;
 
 
 class BrogController extends Controller
@@ -142,12 +142,9 @@ class BrogController extends Controller
     public function subscript()
     {
         $user = Auth::user();
-        return view('subscription.subscript', [
-            'intent'       => $user->createSetupIntent(),
-            // 現在のユーザーに紐づいているサブスクリプション
-            'userProducts' => $user->products(),
-            // dashboardで作成されているサブスクリプション全件
-            'products'     => Price::getAll(),
+    
+        return view('home', [
+            'intent' => $user->createSetupIntent()
         ]);
     }
 }
