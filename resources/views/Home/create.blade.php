4eb7843485bcdfcf3f7e93f7981282f729cf70a8
Tenta-Kondo <tenta1010verde@gmail.com>
2021-05-10 19:45:43 +0900

    debug

--- resources/views/Home/create.blade.php
+++ resources/views/Home/create.blade.php
@@ -1,18 +1,21 @@
 <?php
 $threadcount = $thread->count();
+
+$url = url()->previous();
 ?>
 @extends("layout")
 @section("content")
 <main>
+    <a href="{{$url}}" style="color:black;margin-left:10px;"><i class="fas fa-arrow-left" style="margin-right: 5px;"></i>前のページへ戻る</a>
     <section>
         <div class="all-contents">
             <h2 style="margin-bottom: 1rem;font-weight:lighter;">スレッド作成</h2>
             <form action="/threadCreating" method="POST" onSubmit="return checkSubmit()" enctype="multipart/form-data" files="true">
                 {{ csrf_field() }}
-                
+
                 <input type="hidden" name="username" value="{{ Auth::user()->name }}">
                 <input type="text" name="title" placeholder="タイトル(必須)">
-                  
+
                 @if($errors->has("title"))
                 <div class="err_msg">
                     {{$errors->first("title")}}
--- resources/views/Home/user-page.blade.php
+++ resources/views/Home/user-page.blade.php
@@ -1,17 +1,18 @@
 @extends("layout")
 @section("content")
 <main>
+    <a href="{{$url}}" style="color:black;margin-left:10px;"><i class="fas fa-arrow-left" style="margin-right: 5px;"></i>前のページへ戻る</a>
     <article class="user-card">
-    <h1 class="page-title">User Page</h1>
-    <div class="user-image"></div>
-    <table class="user-table">
-        <tr>
-            <td>User Name(Apelido) : {{ Auth::user()->name }}</td>
-        </tr>
-        <tr>
-            <td>Mail Adress : {{ Auth::user()->email }}</td>
-        </tr>
-    </table>
+        <h1 class="page-title">User Page</h1>
+        <div class="user-image"></div>
+        <table class="user-table">
+            <tr>
+                <td>User Name(Apelido) : {{ Auth::user()->name }}</td>
+            </tr>
+            <tr>
+                <td>Mail Adress : {{ Auth::user()->email }}</td>
+            </tr>
+        </table>
     </article>
 </main>
 @endsection
