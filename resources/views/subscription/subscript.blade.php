9ab985a7233cebb1c1794078723b6b7715fa57f8
Tenta-Kondo <tenta1010verde@gmail.com>
2021-06-04 21:04:58 +0900

    update

--- resources/views/subscription/subscript.blade.php
+++ resources/views/subscription/subscript.blade.php
@@ -1,6 +1,8 @@
-<html>
+<!DOCTYPE html>
+<html lang="ja">
 
 <head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
     <style>
