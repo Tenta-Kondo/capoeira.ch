892f563a0441c6dfa959f4cdb032cf1b05d01200
Tenta-Kondo <tenta1010verde@gmail.com>
2021-07-01 15:51:35 +0900

    update

--- resources/views/subscription/subscriptCreate.blade.php
+++ resources/views/subscription/subscriptCreate.blade.php
@@ -41,7 +41,33 @@
                         <span>有料会員</span>
                         @endif
                     </li>
-                   
+                    <li class="list-group-item">
+
+                        @if($user->stripe_id || $defaultCard2 )
+                        @if(!$user->status)
+
+                        <form action="/user/paid" method="POST" onSubmit="return subscCheckSubmit()">
+                            @csrf
+                            <button id="" type="submit" class="btn btn-primary">有料会員登録はこちら</button>
+                        </form>
+                        <form action="/delete/card" method="POST">
+                            @csrf
+                            <button id="" type="submit" class="btn btn-primary">カード情報削除はこちら</button>
+                        </form>
+                        @elseif($user->status === 1)
+                        <p>下記ボタンを押すことで、有料会員を解約することができます。再度有料会員になる際は新たにお支払いが必要になりますので、ご注意ください。</p>
+                        <form action="/user/cancel" method="POST" onSubmit="return cancelCheckSubmit()">
+                            @csrf
+                            <button id="" class="btn btn-danger">有料会員を解約する</button>
+                        </form>
+                        @endif
+
+                        @else
+                        <p>有料会員登録をするためには、クレジットカード登録を行っていただく必要があります。登録した後に有料会員申し込みの手続を行ってください。</p>
+                        <button type="btn" class="btn" onclick="location.href='/user/payment/form'">カード登録はこちら</button>
+                        @endif
+
+                    </li>
                 </ul>
             </div>
 
