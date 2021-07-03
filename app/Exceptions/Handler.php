9773d07d2ab783a2bb3900c128536544f7782849
Tenta-Kondo <tenta1010verde@gmail.com>
2021-07-03 17:50:07 +0900

    update

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,9 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Throwable;
-
+use Illuminate\Support\Facades\Auth;
 class Handler extends ExceptionHandler
 {
     /**
@@ -36,5 +37,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    public function render($request, Throwable $exception)
+    {
+        // TokenMismatchException 例外発生時
+        if ($exception instanceof \Illuminate\Session\TokenMismatchException) {
+            // ログアウトリクエスト時は、強制的にログアウト
+            if ($request->is('logout')) {
+                Auth::logout();
+            }
+        }
+
+        return parent::render($request, $exception);
     }
 }
